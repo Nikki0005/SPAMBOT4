59ac3f0c968a9e280f211f0a24c72d3301cd09c4
ENGINEER272 <104164168+ENGINEER272@users.noreply.github.com>
2022-04-27 03:36:18 +0530

    Update Engineer.py

--- Engineer.py
+++ Engineer.py
@@ -689,28 +689,28 @@ async def spam(e):
         smex = await e.get_reply_message()
         if len(yukki) == 2:
             message = str(yukki[1])
-            counter = int(yukki[5])
+            counter = int(yukki[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     if e.reply_to_msg_id:
                         await smex.reply(message)
                     else:
                         await e.client.send_message(e.chat_id, message)
-                    await asyncio.sleep(0.001)
+                    await asyncio.sleep(0.005)
         elif e.reply_to_msg_id and smex.media:  
-            counter = int(yukki[5])
+            counter = int(yukki[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "document"):
                     smex = await e.client.send_file(e.chat_id, smex, caption=smex.text)
                     await gifspam(e, smex) 
-                await asyncio.sleep(0.001)  
+                await asyncio.sleep(0.005)  
         elif e.reply_to_msg_id and smex.text:
             message = smex.text
-            counter = int(yukki[5])
+            counter = int(yukki[0])
             for _ in range(counter):
                 async with e.client.action(e.chat_id, "typing"):
                     await e.client.send_message(e.chat_id, message)
-                    await asyncio.sleep(0.001)
+                    await asyncio.sleep(0.005)
         else:
             await e.reply(usage, parse_mode=None, link_preview=None )
 
